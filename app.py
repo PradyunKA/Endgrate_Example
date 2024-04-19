a11bd3d1e8c2f6cf9c0d1420f86674f4f7ae131a
himanshumanchandia <126728919+himanshumanchandia@users.noreply.github.com>
2024-04-18 20:45:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ def home():
 
 # Main function to run the Flask app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Set host to '0.0.0.0'
+    app.run(host='0.0.0.0', port=10000)  # Set host to '0.0.0.0'
